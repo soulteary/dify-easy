0a44be79fb6aa499995f881fba6754d0d089e27b
Su Yang <soulteary@gmail.com>
2024-09-07 10:40:32 +0800

    chore: add fn helper

---
+++ fn/fn.go
@@ -0,0 +1,11 @@
+package Fn
+
+import "strings"
+
+func FixYAML(raw []byte) string {
+	output := string(raw)
+	output = strings.ReplaceAll(output, "<<: '*shared-api-worker-env'", "<<: *shared-api-worker-env")
+
+	output = strings.ReplaceAll(output, `: keep-empty`, ":")
+	return output
+}
