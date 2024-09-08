4bb4c3c40db94b2f0a040836bf79d12d9a69d244
Su Yang <soulteary@gmail.com>
2024-09-08 23:40:45 +0800

    chore: align command and entrypoint with official configuration indentation

--- fn/fn.go
+++ fn/fn.go
@@ -17,11 +17,25 @@ func FixYAML(raw string) string {
 	output = strings.ReplaceAll(output, "command: |-\n      >", "command: >")
 	// fix entrypoint command
 	output = regexp.MustCompile(`(?m)(entrypoint:\s)\|\n\s+(\[.*\])`).ReplaceAllString(output, "$1$2")
+	output = regexp.MustCompile(`(?m)(entrypoint:\s)\|\n\s+(\[.*\])`).ReplaceAllString(output, "$1$2")
+	for _, match := range regexp.MustCompile(`(?m)^\s+entrypoint:\s\[(.+)\]$`).FindAllStringSubmatch(output, -1) {
+		sub := regexp.MustCompile(`(?m),`).ReplaceAllString(match[0], ", ")
+		sub = regexp.MustCompile(`(?m)\[(.*)\]`).ReplaceAllString(sub, "[ $1 ]")
+		sub = strings.ReplaceAll(sub, "\\\\", "\\")
+		output = strings.ReplaceAll(output, match[0], sub)
+	}
+
 	// fix command
 	output = regexp.MustCompile(`(?m)(command:\s)\|\n\s+(\[.*\])`).ReplaceAllString(output, "$1$2")
+	output = regexp.MustCompile(`(?m)(command:\s)\|\n\s+(\[.*\])`).ReplaceAllString(output, "$1$2")
+	for _, match := range regexp.MustCompile(`(?m)^\s+command:\s\[(.+)\]$`).FindAllStringSubmatch(output, -1) {
+		sub := regexp.MustCompile(`(?m),`).ReplaceAllString(match[0], ", ")
+		sub = regexp.MustCompile(`(?m)\[(.*)\]`).ReplaceAllString(sub, "[ $1 ]")
+		output = strings.ReplaceAll(output, match[0], sub)
+	}
+
 	// fix healthcheck command
 	output = regexp.MustCompile(`(?m)(test:\s)\|\n\s+(\[.*\])`).ReplaceAllString(output, "$1$2")
-
 	for _, match := range regexp.MustCompile(`(?m)^\s+test:\s\[(.+)\]$`).FindAllStringSubmatch(output, -1) {
 		sub := regexp.MustCompile(`(?m),`).ReplaceAllString(match[0], ", ")
 		sub = regexp.MustCompile(`(?m)\[(.*)\]`).ReplaceAllString(sub, "[ $1 ]")
